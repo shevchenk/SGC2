850ee16008549ec9effdbdc810622585be258cb2
shevchenk <jorgeshevchenk@gmail.com>
2019-03-29 18:07:57 -0500

    act reporte

--- reporte/excel/EXCELpuntajesPostulantes.php
+++ reporte/excel/EXCELpuntajesPostulantes.php
@@ -230,13 +230,25 @@ $objPHPExcel->getActiveSheet()->setTitle('Reporte_Consolidado_Masivo');
 $objPHPExcel->setActiveSheetIndex(0);
 PHPExcel_Calculation::getInstance()->cyclicFormulaCount = 1;
 
-// Redirect output to a client's web browser (Excel2007)
+/*// Redirect output to a client's web browser (Excel2007)
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment;filename="Reporte_Consolidado_Masivo_'.date("Y-m-d_H-i-s"). " - ".$nombreReporte .'.xlsx"');
 header('Cache-Control: max-age=0');
 
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-$objWriter->save('php://output');
+$objWriter->save('php://output');*/
+header('Content-Type: application/vnd.ms-excel');
+            header('Content-Disposition: attachment;filename="Reporte_Consolidado_Masivo_'.date("Y-m-d_H-i-s").'.xls"'); // file name of excel
+            header('Cache-Control: max-age=0');
+            // If you're serving to IE 9, then the following may be needed
+            header('Cache-Control: max-age=1');
+            // If you're serving to IE over SSL, then the following may be needed
+            header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+            header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+            header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+            header ('Pragma: public'); // HTTP/1.0
+            $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+            $objWriter->save('php://output');
 exit;
 
 } catch (Exception $e) {
